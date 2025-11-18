cdf9a2fb29f9960b3ae26ba76311c29a28c05b40
GSlM276 <GabrielSlMorais@outlook.com>
2025-11-18 01:41:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.alert('pagina em desenvimento')
+
+
+function clicar(projeto) {
+    if (projeto === 'niceday') {
+        window.location.href = 'https://www.google.com';
+    }
+    else if (projeto === 'imc') {
+        window.location.href = 'https://gslm276.github.io/IMC/'; 
+    }
+    else if (projeto === 'ecommerce') {
+        window.location.href = 'https://exemplo.com/ecommerce';
+    }
+    else if (projeto === 'lista') {
+        window.location.href = 'https://exemplo.com/lista';
+    }
+}
